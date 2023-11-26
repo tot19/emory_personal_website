90e32f60f9127d86724b3f55d8e02abdebb4ae2a
Emory Otott <emoryotott19@gmail.com>
2023-11-26 04:14:51 -0500

    finished header

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
