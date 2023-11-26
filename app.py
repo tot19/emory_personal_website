d4410baf966fc5403b501b8f588278d23fa7d70b
Emory Otott <emoryotott19@gmail.com>
2023-11-26 03:11:06 -0500

    added base layout

--- app.py
+++ app.py
@@ -12,15 +12,5 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
---
+++ static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+        duration: 1200, // Animation duration
+        once: true, // Whether animation should happen only once - while scrolling down
+    });
+});
